bddb79ce2774267d6f04941addb25c4e3b5bd02f
Harshdeep <harshdeep.singh@tothenew.com>
2019-02-14 13:19:02 +0530

    Question 8 doubt resolved

--- introduction_to_jquery/q8/app.js
+++ introduction_to_jquery/q8/app.js
@@ -3,19 +3,19 @@ $(() => {
     // $('#my-button').after(element);
 });
 
-// $('.dy-button').on("click",function(){
-//     console.log('event');
-// });
+$('.container').on("click",'.dy-button',function($event){
+    var myhtml = $(this).html();
+    $('.footer p').append(" " + myhtml + " ");
+});
 
+// $('.dy-button').click(($event) => {
+//     console.log(event);
+// });
 
 $('#my-button').on('click',($event) => {
     console.log(event);
     var element = "<button class='dy-button'>I am dynamically generated</button>"
     $('#my-button').after(element);
-    
-    $('.dy-button').click(($event) => {
-        console.log(event);
-    });
 });
 
 
