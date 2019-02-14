619e9daac80fe81fd7282c828f721166bf652cfe
Harshdeep <harshdeep.singh@tothenew.com>
2019-02-14 10:50:42 +0530

    Question 8 Doubt Doubt

--- introduction_to_jquery/q8/app.js
+++ introduction_to_jquery/q8/app.js
@@ -3,19 +3,21 @@ $(() => {
     // $('#my-button').after(element);
 });
 
-$(' .dy-button').on("click",function(){
-    console.log('event');
-});
+// $('.dy-button').on("click",function(){
+//     console.log('event');
+// });
 
 
 $('#my-button').on('click',($event) => {
     console.log(event);
     var element = "<button class='dy-button'>I am dynamically generated</button>"
     $('#my-button').after(element);
-})
+    
+    $('.dy-button').click(($event) => {
+        console.log(event);
+    });
+});
 
-//this wont work
-// $('#d-button').click(($event) => {
-//     console.log(event);
-// })
+
+
 
