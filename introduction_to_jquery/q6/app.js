b67a95eb8471812f99d260e69d100f8d242ceafa
Harshdeep <harshdeep.singh@tothenew.com>
2019-02-17 03:10:54 +0530

    todo-replaced tag

--- introduction_to_jquery/q6/app.js
+++ introduction_to_jquery/q6/app.js
@@ -1,9 +1,6 @@
 $('button').click(($vent) => {
     // var div_content = $('div').html("<p>This is 'p' tag.</p>");
-    $('body .mydiv').replaceWith(function(){
-        return ($('<p></p>'),{
-            html: $(this).html(),
-            
-        })
+    $('.mydiv').replaceWith(function(){
+        return ($('<p/>').append($(this).html()));
     });
 })
