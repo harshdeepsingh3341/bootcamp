620ef463b7008066b802163621e15d919a3aaaac
Harshdeep <harshdeep.singh@tothenew.com>
2019-03-02 00:49:21 +0530

    exercise complete

---
+++ page_layouts_and_structure_two/assets/scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        directionNav: false,
+        controlNav: false,
+        slideshowSpeed: 1000,
+        animationSpeed: 2000
+    });
+});
