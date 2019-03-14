e04f48c2a4c6556743c489ad57b4231b83dbe007
Harshdeep <harshdeep.singh@tothenew.com>
2019-03-14 13:35:46 +0530

    optimized code

--- basicsJS/assets/scripts/app.js
+++ basicsJS/assets/scripts/app.js
@@ -1,77 +1,61 @@
-var clock = function(){
-    var date = new Date();
-
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var seconds = date.getSeconds();
-
+var greeting = function(hour) {
     var greet_message = "good ";
+    if (hour < 12 && hour >=0) {
+        greet_message += "morning";
+    }
+    else if(hour >=12 && hour < 16){
+        greet_message += "afternoon";
+    }
+    else {
+        greet_message += "evening";
+    }
+    return greet_message;
+}
 
-    var time_message, date_message;
-
-    var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sept", "oct", "nov","dec"];
-
+var time = function(hour, minute, seconds){
+    var time_message;
     if(hour<12){
-
-        if (hour < 12 && hour >=0) {
-            greet_message += "morning";
-        }
-
         if(hour == 0) hour = 12;
 
         time_message = {
-            hour: hour,
-            minute: minute,
-            seconds: seconds,
+            hour: (hour<=9?"0"+hour:hour),
+            minute: (minute<=9?"0"+minute:minute),
+            seconds: (seconds<=9?"0"+seconds:seconds),
             extra: "am"
         }
-        
     }
     else {
-        if(hour >=12 && hour < 16){
-            greet_message += "afternoon";
-        }
-        else {
-            greet_message += "evening";
-        }
-        
         time_message = {
-            hour: hour-12,
-            minute: minute,
-            seconds: seconds,
+            hour: ((hour-12)<=9?"0"+(hour-12):(hour-12)),
+            minute: (minute<=9?"0"+minute:minute),
+            seconds: (seconds<=9?"0"+seconds:seconds),
             extra: "pm"
         }
-
-        if(time_message.hour == 0){
-            time_message.hour = 12;
+        if(time_message.hour == "00"){
+            time_message.hour = '12';
         }
     }
+    return time_message.hour + ":" + time_message.minute + ":" + time_message.seconds + " " + time_message.extra;
+}
 
-    document.getElementById('greet').innerText = greet_message;
-    
-    if(time_message.hour<=9){
-       
-        time_message.hour = "0" + time_message.hour;
-    }
-    if(time_message.minute<=9){
-        time_message.minute = "0" + time_message.minute;
-    }
-    if(time_message.seconds<=9){
-        time_message.seconds = "0" + time_message.seconds;
-    }
-
-    document.getElementById('time').innerText=time_message.hour + ":" + time_message.minute + ":" + time_message.seconds + " " + time_message.extra;
-
-    date_message = {
+var my_date = function(date){
+    var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sept", "oct", "nov","dec"];
+    var date_message = {
         date: date.getDate(),
         month: months[date.getMonth()],
         year: date.getFullYear()
     }
-
-    document.getElementById('date').innerText = date_message.date + " " + date_message.month + " " + date_message.year;
-
-
-
+    return date_message.date + " " + date_message.month + " " + date_message.year;
 }
 
-setInterval(clock, 100);
+var page = function(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var seconds = date.getSeconds();
+    document.getElementById('greet').innerText = greeting(hour);
+    document.getElementById('time').innerText= time(hour, minute, seconds);
+    document.getElementById('date').innerText = my_date(date);
+}
+
+setInterval(page, 100);
