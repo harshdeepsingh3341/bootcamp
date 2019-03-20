b8bbd49e3feb760063597e8cbfd53df85220bb02
Harshdeep <harshdeep.singh@tothenew.com>
2019-03-20 13:22:54 +0530

    Practice app

---
+++ introduction-to-reactjs/practice/practice-app/src/compoenets/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './main.css'
+
+class Main extends Component{
+    render(){
+        return (
+            <div className="main">
+                main
+            </div>
+        )
+    }
+}
+
+export default Main;
