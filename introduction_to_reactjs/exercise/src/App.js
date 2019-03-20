57684534e970bf9f147222cb119810ea652dd8b8
Harshdeep <harshdeep.singh@tothenew.com>
2019-03-20 23:02:49 +0530

    Added footer component

--- introduction_to_reactjs/exercise/src/App.js
+++ introduction_to_reactjs/exercise/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/header-component/Header";
 import Main from "./components/main-component/Main";
+import Footer from './components/footer-component/Footer'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
           <Header></Header>
           <Main></Main>
+          <Footer></Footer>
       </div>
     );
   }
---
+++ introduction_to_reactjs/exercise/src/components/footer-component/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './footer.css';
+
+class Footer extends Component{
+    render(){
+        return (
+            <footer>
+                FOOTER
+            </footer>
+        )
+    }
+}
+
+export default Footer;
