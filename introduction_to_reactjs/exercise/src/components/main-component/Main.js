ceb4f7bcff4b0b88f4f7bbf5a095b2763b94c5e1
Harshdeep <harshdeep.singh@tothenew.com>
2019-03-20 17:00:26 +0530

    updated .gitignore

---
+++ introduction_to_reactjs/exercise/src/components/main-component/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './main'
+
+class Main{
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <div className="main-component">
+
+            </div>
+        )
+    }
+}
+
+export default Main;
