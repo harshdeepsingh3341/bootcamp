669cd8bacc4daa65deae866d4fdf534be7531b2b
Harshdeep <harshdeep.singh@tothenew.com>
2019-04-04 15:44:53 +0530

    implemented localstorage

--- react_components_state_props/prop_manipulation/src/App.js
+++ react_components_state_props/prop_manipulation/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Dishes from "./components/dishes-component/Dishes";
 import AddDish from "./components/add-dish-component/AddDish";
@@ -6,39 +6,49 @@ import AddDish from "./components/add-dish-component/AddDish";
 class App extends Component {
 
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: [
-        {name: 'Rajma Chawal 1', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
-        {name: 'Rajma Chawal 2', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
-        {name: 'Rajma Chawal 3', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
-        {name: 'Rajma Chawal 4', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']}
-      ]
+    constructor(props) {
+        super(props);
+
+        localStorage.setItem('dishes', JSON.stringify( [
+            {name: 'Rajma Chawal 1', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
+            {name: 'Rajma Chawal 2', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
+            {name: 'Rajma Chawal 3', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
+            {name: 'Rajma Chawal 4', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']}
+        ]));
+
+        console.log(localStorage);
+
+        this.state = {
+            dishes: [
+                {name: 'Rajma Chawal 1', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
+                {name: 'Rajma Chawal 2', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
+                {name: 'Rajma Chawal 3', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']},
+                {name: 'Rajma Chawal 4', cost: 100, ingredients: ['rajma beans', 'rice', 'onion', 'tomato']}
+            ]
+        }
     }
-  }
 
-  addDish = (dish) => {
-      let {name, cost, ingredients} = dish;
-      console.log(name, cost, ingredients);
-      
-      this.setState({
-          dishes: [
-              ...this.state.dishes,
-              {name: name, cost: cost, ingredients: ingredients.split(/,+\s*/)}
-          ]
-      });
-  };
+    addDish = (dish) => {
+        let {name, cost, ingredients} = dish;
+        console.log(name, cost, ingredients);
 
-  render() {
-    return (
-      <div className="App">
-          <h1>Menu</h1>
-          <Dishes list_dishes={this.state.dishes}/>
-          <AddDish callback={this.addDish}/>
-      </div>
-    );
-  }
+        this.setState({
+            dishes: [
+                ...this.state.dishes,
+                {name: name, cost: cost, ingredients: ingredients.split(/,+\s*/)}
+            ]
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Menu</h1>
+                <Dishes list_dishes={this.state.dishes}/>
+                <AddDish callback={this.addDish}/>
+            </div>
+        );
+    }
 }
 
 export default App;
