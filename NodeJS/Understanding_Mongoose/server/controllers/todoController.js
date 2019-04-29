60a153f45a505aaca5161458c0e143216e7bbddf
Harshdeep <harshdeep.singh@tothenew.com>
2019-04-29 22:44:40 +0530

    backend APIs completed

---
+++ NodeJS/Understanding_Mongoose/server/controllers/todoController.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const {
+    todoService:
+        {
+            getTodos,
+            addNewTodo,
+            toggleCheck,
+            editTodo,
+            deleteTodo
+        }
+} = require('../services');
+
+router.get('/:todoGroupId/todos', (req, res, next) => {
+    getTodos(req.params)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(response => {
+            next(response)
+        })
+});
+
+router.post('/:todoGroupId/todo', (req, res, next) => {
+    console.log(req.params, req.body);
+    addNewTodo({...req.params, ...req.body})
+        .then(response => {
+            res.send(response)
+        })
+        .catch(response => {
+            next(response)
+        })
+});
+
+router.put('/:todoGroupId/:todoId/toggleCheck/:checked', (req, res, next) => {
+    toggleCheck(req.params)
+        .then(response => {
+            res.send(response);
+        })
+        .catch(response => {
+            next(response)
+        })
+});
+
+router.put('/:todoGroupId/:todoId/editTodo', (req, res, next) => {
+    editTodo({
+        ...req.params,
+        ...req.body
+    })
+        .then(response => {
+            res.send(response);
+        })
+        .catch(response => {
+            next(response);
+        })
+});
+
+router.delete('/:todoGroupId/:todoId', (req, res, next) => {
+    deleteTodo(req.params)
+        .then(response => {
+            res.send(response);
+        })
+        .catch(response => {
+            next(response);
+        });
+});
+
+module.exports = router;
---
+++ NodeJS/Understanding_Mongoose/server/middlewares/utils.js
@@ -0,0 +1,29 @@
+exports.loggerMiddleWare = (req, res, next) => {
+    console.log(`request received on ${req.url}, with method ${req.method}`);
+    next();
+};
+exports.allowCrossOriginMiddleware = (req, res, next) => {
+    console.log('cors');
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+};
+
+exports.addDelayMiddleware = (req, res, next) => {
+    setTimeout(() => next(), 300)
+};
+
+exports.pageErrorMiddleware = (req, res, next) => {
+    req.status = 404;
+    next(new Error("404 Page not found"))
+};
+
+exports.defaultErrorHandler = (err, req, res, next) => {
+    res.json({
+        status: req.status,
+        message: err.message,
+        stack: err.stack
+    });
+};
